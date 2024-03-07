4e62b6b8ec9a4f470386ab49cdeb6d647eb6d90d
Naledi El Cheikh <elchei_n@etna-alternance.net>
2024-03-07 18:59:48 +0100

    add : json

--- mobile_app/pages/todo.tsx
+++ mobile_app/pages/todo.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
-import Task from './types'
+import Task from '../types'
 import { View, Text, StyleSheet, TextInput, Button, TouchableOpacity, ScrollView } from 'react-native'
 
 export default function Home() {
-  const [tasks, setTasks] = useState([{ id: 0, name: 'Tâche test', isChecked: true }])
+  const [tasks, setTasks] = useState([
+    { id: 0, name: 'Commencer les décorations de Noel', isChecked: true, modified: false }
+  ])
   const [inputTask, setInputTask] = useState('')
   const [updatedTask, setUpdatedTask] = useState('')
 
@@ -24,7 +26,7 @@ export default function Home() {
   const addItem = () => {
     if (inputTask.trim() !== '') {
       const newId = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1
-      setTasks([...tasks, { id: newId, name: inputTask, isChecked: false }])
+      setTasks([...tasks, { id: newId, name: inputTask, isChecked: false, modified: false }])
       setInputTask('')
     }
   }
@@ -33,23 +35,37 @@ export default function Home() {
     setTasks(tasks.map((task) => (task.isChecked ? task : { ...task, isChecked: !task.isChecked })))
   }
 
+  const updateTask = (id: number, newName: string, task: any) => {
+    task.modified = !task.modified
+    const updatedTask = tasks.map((task) => (task.id === id ? { ...task, name: newName } : task))
+    setTasks(updatedTask)
+  }
+
   return (
     <View style={{ flex: 1 }}>
       <ScrollView>
         <View>
-          <Text style={styles.heading}>Today's tasks</Text>
+          <Text style={styles.heading}>ONGOING</Text>
           {tasks.map((task, index) => (
             <View key={task.id} style={styles.taskBlock}>
-              <Button onPress={() => handleCheckboxChange(task.id)}>Done</Button>
-              <Text style={styles.taskName}>{task.name}</Text>
-
+              <TouchableOpacity onPress={() => handleCheckboxChange(task.id)}>
+                <Text style={styles.taskName}>{task.name}</Text>
+              </TouchableOpacity>
+              {task.modified && (
+                <TextInput
+                  style={styles.input}
+                  value={updatedTask}
+                  onChangeText={(text) => setUpdatedTask(text)}
+                  placeholder="Update task..."
+                />
+              )}
               <View style={styles.buttons}>
+                <Button title="Update" onPress={() => updateTask(task.id, updatedTask, task)} />
                 <Button title="Remove" onPress={() => removeItem(index)} />
               </View>
             </View>
           ))}
         </View>
-        <Text style={styles.heading}>Done !</Text>
       </ScrollView>
       <View style={styles.inputContainer}>
         <TextInput
@@ -60,7 +76,6 @@ export default function Home() {
         />
         <Button title="Add" onPress={addItem} />
       </View>
-
       <View style={styles.footer}>
         <Button title="All done" onPress={() => handleAllCheckboxChange()} />
         <Button title="Delete all" onPress={() => removeAllItems()} />
@@ -73,8 +88,7 @@ const styles = StyleSheet.create({
   heading: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginTop: 40,
-    marginBottom: 15
+    marginBottom: 10
   },
   taskBlock: {
     flexDirection: 'row',
---
+++ mobile_app/types.ts
@@ -0,0 +1,7 @@
+interface Task {
+  id: number
+  name: string
+  isChecked: boolean
+}
+
+export default Task
