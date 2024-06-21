21adb22eb6ba619d31e6e0a0f00fb8c5a48d4a59
Naledi El Cheikh <elchei_n@etna-alternance.net>
2024-06-21 17:08:57 +0200

    add : library app

--- BlissValley/pages/library/AppLibrary.tsx
+++ BlissValley/pages/library/AppLibrary.tsx
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageBackground, TouchableOpacity } from 'react-native'
+import JournalLogo from '../../components/JournalLogo'
 
-export default function AppLibrary() {
+export default function AppLibrary({navigation}) {
   return (
     <View style={styles.container}>
-      <Text>App Library</Text>
+      <ImageBackground source={require('../../assets/pngs/library.png')} resizeMode="cover" style={styles.image}>
+      
+        <TouchableOpacity onPress={() => navigation.navigate("Journal")}>
+          <JournalLogo  />
+          <Text style={styles.appName}>Journal</Text>
+        </TouchableOpacity>
+        <View style={{width : '100%', height : '100%', opacity : 0.3, backgroundColor : '#cf69ff'}}>
+      </View>
+      </ImageBackground>
     </View>
   )
 }
@@ -14,5 +23,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  }, 
+  image : {
+    flex : 1, 
+    width : '100%'
+  }, 
+  appName : {
+
   }
 })
